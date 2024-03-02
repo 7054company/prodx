1fb5fcb10584e76a16aee60692befc0b3b3a9ddd
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-03-02 17:36:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 // Define route for the main page
+// app.get('/', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // Define route for fetching IP details by providing any IP address
