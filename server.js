adb448af6951ee027a3584c71b6241269f28cb6e
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-03-02 17:39:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.use(express.static('public'));
 // app.get('/', (req, res) => {
 //  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + 'public/index.html');
 });
 
 // Define route for fetching IP details by providing any IP address
