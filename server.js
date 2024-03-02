de07303aea641c40d7e03742d119ac7a427a6342
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-03-02 18:53:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const apiRouter = require('./api');
 const axios = require('axios');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/api', apiRouter);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
