633d60cb42869a7c3e0a90c420941d56160495d7
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-03-04 22:45:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const apiRouter = require('./api');
-const authRouter = require('./auth');
-// const bodyParser = require('body-parser');
 const axios = require('axios');
 const path = require('path');
-// const jwt = require('jsonwebtoken');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON and URL-encoded data
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static files (e.g., login.html) from the 'views' directory
-// app.use(express.static(path.join(__dirname, 'views')));
-
-// Use the apiRouter for '/api' endpoints
 app.use('/api', apiRouter);
-
-// Use the authRouter for '/auth' endpoints
-app.use('/auth', authRouter);
-
-// Define a route for the root ('/') endpoint
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
