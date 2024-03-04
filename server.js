8268ad1ac04337cae7c8f8d9875b6023dd9dcf9e
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-03-04 23:35:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apiRouter = require('./api');
 const authRouter = require('./auth');
+const dashboardRouter = require('./api/dashboard');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const path = require('path');
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
+app.use('/api', dashboardRouter);
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
@@ -21,6 +24,9 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+});
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
