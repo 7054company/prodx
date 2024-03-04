4b9f3521cb0dd0d3654d1ea295263bb848ed187f
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-03-04 22:52:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apiRouter = require('./api');
 const authRouter = require('./auth');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 const path = require('path');
 
