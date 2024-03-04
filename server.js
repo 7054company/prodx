336474aad46d3384d77bbc2c0dc6f20ba6a1a081
VIKAS YADAV <34761088+7054company@users.noreply.github.com>
2024-03-04 23:16:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use('/api', apiRouter);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
